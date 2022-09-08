2978e5224ac1f75d476d8c404e1ce6469bdce736
Hosoya <choukaisanw0626@gmail.com>
2022-09-08 22:25:49 +0900

    　

---
+++ database/migrations/2022_07_06_131808_create_store_reviews_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStoreReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('store_reviews', function (Blueprint $table) {
+            $table->increments('id')->comment('投稿番号');
+            $table->string('store_name')->comment('店舗名');
+            $table->string('place')->comment('場所');
+            $table->string('regular')->comment('常連');
+            $table->string('TV')->comment('テレビ');
+            $table->string('temperature')->comment('室内温度');
+            $table->string('water_temperature')->comment('水風呂温度');
+            $table->string('width')->comment('広さ');
+            $table->string('smell')->comment('匂い');
+            $table->string('congestion')->comment('混雑度');
+            $table->string('outside_bath')->comment('外気浴');
+            $table->string('music')->comment('音楽');
+            $table->string('mat')->comment('サウナマット');
+            $table->string('cleanliness')->comment('清潔度');
+            $table->string('age group')->comment('年齢層');
+            $table->string('review')->comment('口コミ');
+            $table->integer('del_flg')->comment('削除フラグ')->default(0);
+            $table->string('user_id')->comment('ユーザーID');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('store_reviews');
+    }
+}
---
+++ database/migrations/2022_07_23_021406_create_experience_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateExperienceTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('experience', function (Blueprint $table) {
+            $table->increments('id')->comment('投稿番号');
+            $table->string('difficulty')->comment('難易度');
+            $table->string('time')->comment('所要時間');
+            $table->string('review')->comment('楽しみ方');
+            $table->integer('del_flg')->comment('削除フラグ')->default(0);
+            $table->integer('user_id')->comment('ユーザーID');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('experience');
+    }
+}
